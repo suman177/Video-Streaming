f66a89177c2378c90b6eae9d3377527d494a55ed
suman177 <sumanregmi177@gmail.com>
2020-12-03 16:02:17 +0530

    Half Baked

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,35 @@
+import 'dart:io';
+import 'dart:math';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:transparent_image/transparent_image.dart';
+import 'apis/encoding_provider.dart';
+import 'apis/firebase_provider.dart';
+import 'package:path/path.dart' as p;
+import 'models/video_info.dart';
+import 'widgets/player.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Video Sharing',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Video Sharing'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,68 +38,114 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  
+  double _progress = 0.0;
+  bool _canceled = false;
+  int _videoDuration = 0;
+  String _processPhase = "";
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
+  void initState() {
     //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    EncodingProvider.enableStatisticsCallback((int time,
+        int size,
+        double bitrate,
+        double speed,
+        int videoFrameNumber,
+        double videoQuality,
+        double videoFps) {
+      
+      if (_canceled) return;
+
+      setState(() {
+        _progress = time / _videoDuration;
+      });
+    });
+
+    super.initState();
+  }
+
+  //Widget to show Progress bar.
+  _getProgressBar() {
+    return Container(
+      padding: EdgeInsets.all(30.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(bottom: 30.0),
+            child: Text(_processPhase),
+          ),
+          LinearProgressIndicator(
+            value: _progress,
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _onUploadProgress(event) {
+    if(event.type == StorageTaskEventType.progress) {
+      final double progess = event.snapshot.byteTransferred / event.snapshot.totalByteCOunt
+    }
+  }
+  //This method takes filepath foldreName as paramater and upload the file to the server
+  //It will make a StorageReference on the server and handle live events and so on.
+  Future<String> _uploadFile(filepath, folderName) async {
+    final file = new File(filepath);
+    final basename = p.basename(filepath);
+
+    final StorageReference ref =
+        FirebaseStorage.instance.ref().child(folderName).child(basename);
+    StorageUploadTask uploadTask = ref.putFile(file);
+    uploadTask.events.listen((_onUploadProgress));
+    StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
+    String videoUrl = await taskSnapshot.ref.getDownloadURL();
+
+    return videoUrl;
+  }
+
+  //This method fixes the HLS file. '.ts' and '.m3u8' files are edited here to point to correct location which is cloud 
+  //after we upload the video.
+  void _updatePlaylistUrls(File file, String videoName) {
+    final lines = file.readAsLinesSync();
+    var updatedLines =List<String>();
+
+    for(final String line in lines) {
+      var updatedLine = line;
+      if(line.contains('.ts') || line.contains('.m3u8')){
+          updatedLine = '$videoName%2F$line?alt=media';
+      }
+      updatedLines.add(updatedLine);
+    }
+
+    final updatedContents = updatedLines.reduce((value, element)=> value + '\n' + element);
+
+    file.writeAsStringSync(updatedContents);
+  }
+
+  Future<String> _uploadHLSFiles(dirPath, videoName) async {
+    
+    final videosDir = Directory(dirPath);
+
+    var playlistUrl = '';
+    
+    final files = videosDir.listSync();
+    int i =1;
+    for (FileSystemEntity file in files) {
+      final fileName = p.basename(file.path);
+      final fileExtension = getFileExtension(fileName);
+
+    }
+
+  }
+
+  //This method gives the extension of file to verify weather to upload or not
+  //example will return .m3u8 if it is m3u8 file.
+  String getFileExtension(String fileName) {
+    final exploded =fileName.split('.');
+    return exploded[exploded.length - 1];
   }
 }
